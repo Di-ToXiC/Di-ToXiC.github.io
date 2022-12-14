65cbbfdf961716aad25699241e8073e06707e2bc
Di-ToXiC <ditoxic1990@yandex.ru>
2022-12-15 01:03:25 +0300

    Final_attestation

---
+++ src/main/java/com/example/springsecurityapplication/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example.springsecurityapplication.config;
+
+//import com.example.springsecurityapplication.security.AuthenticationProvider;
+import com.example.springsecurityapplication.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PersonDetailsService personDetailsService;
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error", "/auth/registration", "/product", "/product/info/{id}", "/img/**", "/product/search", "/G:/SpringSecurityApplication 3/src/main/resources/static/img").permitAll()
+                .anyRequest().hasAnyRole("USER", "ADMIN")
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/index", true)
+                .failureUrl("/auth/login")
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login");
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/icon/**");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
---
+++ src/main/java/com/example/springsecurityapplication/controllers/AdminController.java
@@ -0,0 +1,161 @@
+package com.example.springsecurityapplication.controllers;
+
+import com.example.springsecurityapplication.models.Image;
+import com.example.springsecurityapplication.models.Product;
+import com.example.springsecurityapplication.repositories.CategoryRepository;
+import com.example.springsecurityapplication.security.PersonDetails;
+import com.example.springsecurityapplication.services.ProductService;
+import com.example.springsecurityapplication.util.ProductValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/admin")
+//@PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+public class AdminController {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    private final ProductValidator productValidator;
+    private final ProductService productService;
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public AdminController(ProductValidator productValidator, ProductService productService, CategoryRepository categoryRepository) {
+        this.productValidator = productValidator;
+        this.productService = productService;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping()
+    public String admin(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+
+        String role = personDetails.getPerson().getRole();
+
+        if(role.equals("ROLE_USER")){
+            return "redirect:/index";
+        }
+        model.addAttribute("products", productService.getAllProduct());
+        return "admin/admin";
+    }
+
+    @GetMapping("/product/add")
+    public String addProduct(Model model){
+        model.addAttribute("product", new Product());
+        model.addAttribute("category", categoryRepository.findAll());
+        return "product/addProduct";
+    }
+
+    @PostMapping("/product/add")
+    public String addProduct(@ModelAttribute("product") @Valid Product product, BindingResult bindingResult, @RequestParam("file_one") MultipartFile file_one, @RequestParam("file_two") MultipartFile file_two, @RequestParam("file_three") MultipartFile file_three, @RequestParam("file_four") MultipartFile file_four, @RequestParam("file_five") MultipartFile file_five) throws IOException {
+
+        productValidator.validate(product, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "product/addProduct";
+        }
+        if(file_one != null){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file_one.getOriginalFilename();
+            file_one.transferTo(new File(uploadPath + "/" + resultFileName));
+            Image image = new Image();
+            image.setProduct(product);
+            image.setFileName(resultFileName);
+            product.addImageProduct(image);
+        }
+        if(file_two != null){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file_two.getOriginalFilename();
+            file_two.transferTo(new File(uploadPath + "/" + resultFileName));
+            Image image = new Image();
+            image.setProduct(product);
+            image.setFileName(resultFileName);
+            product.addImageProduct(image);
+        }
+        if(file_three != null){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file_three.getOriginalFilename();
+            file_three.transferTo(new File(uploadPath + "/" + resultFileName));
+            Image image = new Image();
+            image.setProduct(product);
+            image.setFileName(resultFileName);
+            product.addImageProduct(image);
+        }
+        if(file_four != null){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file_four.getOriginalFilename();
+            file_four.transferTo(new File(uploadPath + "/" + resultFileName));
+            Image image = new Image();
+            image.setProduct(product);
+            image.setFileName(resultFileName);
+            product.addImageProduct(image);
+        }
+        if(file_five != null){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file_five.getOriginalFilename();
+            file_five.transferTo(new File(uploadPath + "/" + resultFileName));
+            Image image = new Image();
+            image.setProduct(product);
+            image.setFileName(resultFileName);
+            product.addImageProduct(image);
+        }
+
+        productService.saveProduct(product);
+        return "redirect:/admin";
+    }
+    @GetMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable("id") int id){
+        productService.deleteProduct(id);
+        return "redirect:/admin";
+    }
+    @GetMapping("/product/edit/{id}")
+    public String editProduct(@PathVariable("id") int id, Model model){
+        model.addAttribute("editProduct", productService.getProductId(id));
+        model.addAttribute("category", categoryRepository.findAll());
+        return "product/editProduct";
+    }
+
+    @PostMapping("/product/edit/{id}")
+    public String editProduct(@ModelAttribute("editProduct") Product product, @PathVariable("id") int id){
+        productService.updateProduct(id, product);
+        return "redirect:/admin";
+    }
+
+
+
+}
